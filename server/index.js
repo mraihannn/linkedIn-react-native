35494dc9c863402373f4635905ec3e7e901530e2
MUHAMMAD RAIHAN <muhammadraihan@mail.ugm.ac.id>
2024-06-24 16:54:13 +0700

    Setup Apollo Server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+
+const {
+  typeDefs: userTypeDefs,
+  resolvers: userResolvers,
+} = require("./schemas/user");
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+  typeDefs: [userTypeDefs],
+  resolvers: [userResolvers],
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+// const { url } = await startStandaloneServer(server, {
+//   listen: { port: 3000 },
+// });
+startStandaloneServer(server, {
+  listen: { port: 3000 },
+}).then(({ url }) => {
+  console.log(`🚀  Server ready at: ${url}`);
+});
