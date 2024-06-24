4e74488e7c5e52fd24a64ddac5375deb59c3e3b3
MUHAMMAD RAIHAN <muhammadraihan@mail.ugm.ac.id>
2024-06-24 20:22:52 +0700

    feat: Install MongoDB

---
+++ server/config/mongodb.js
@@ -0,0 +1,26 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://mraihann0:dvHerPJY5dbbwwDU@cluster0.0pnhhtg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
