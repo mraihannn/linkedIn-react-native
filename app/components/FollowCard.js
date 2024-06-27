916f82aa294c31b6da1b5f066aa2d079329200f7
MUHAMMAD RAIHAN <muhammadraihan@mail.ugm.ac.id>
2024-06-27 19:09:42 +0700

    feat: Search Screen

--- app/components/FollowCard.js
+++ app/components/FollowCard.js
@@ -8,25 +8,52 @@ export default function FollowCard() {
         borderBottomColor: "#e9e5df",
         paddingVertical: 25,
         flexDirection: "row",
-        gap: 10,
+        justifyContent: "space-between",
+        alignItems: "center",
       }}
     >
-      <View
+      <View style={{ flexDirection: "row", gap: 10 }}>
+        <View
+          style={{
+            width: 50,
+            height: 50,
+            backgroundColor: "#e7a33e",
+            borderRadius: 100,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ fontSize: 25, color: "white" }}>N</Text>
+        </View>
+        <View style={{ justifyContent: "space-between" }}>
+          <Text style={{ fontSize: 17, fontWeight: 500 }}>Fullname</Text>
+          <Text style={{ fontSize: 17, fontWeight: 400 }}>Username</Text>
+        </View>
+      </View>
+      <Text
         style={{
-          width: 50,
-          height: 50,
-          backgroundColor: "#e7a33e",
-          borderRadius: 100,
-          justifyContent: "center",
-          alignItems: "center",
+          backgroundColor: "#0a66c2",
+          color: "white",
+          fontWeight: "bold",
+          borderRadius: 20,
+          paddingHorizontal: 14,
+          paddingVertical: 7,
         }}
       >
-        <Text style={{ fontSize: 25, color: "white" }}>N</Text>
-      </View>
-      <View style={{ justifyContent: "space-between" }}>
-        <Text style={{ fontSize: 17, fontWeight: 500 }}>Fullname</Text>
-        <Text style={{ fontSize: 17, fontWeight: 400 }}>Username</Text>
-      </View>
+        Follow
+      </Text>
+      <Text
+        style={{
+          backgroundColor: "#e9e5df",
+          color: "#56687a",
+          fontWeight: "bold",
+          borderRadius: 20,
+          paddingHorizontal: 14,
+          paddingVertical: 7,
+        }}
+      >
+        Followed
+      </Text>
     </View>
   );
 }
--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,17 +1,61 @@
 import React from "react";
-import { FlatList, StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, Text, TextInput, View } from "react-native";
 import Card from "../components/Card";
+import FollowCard from "../components/FollowCard";
 
 export default function HomeScreen({ route, navigation }) {
   // const { message } = route.params;
+  const [search, setSearch] = React.useState("");
   const DATA = [1, 2, 3, 4, 5, 6, 7];
   return (
     <View style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={() => <Card />}
-        keyExtractor={(item) => item.id}
-      />
+      <View
+        style={{
+          backgroundColor: "white",
+          flexDirection: "row",
+          alignItems: "center",
+          paddingVertical: 5,
+          paddingHorizontal: 16,
+          marginBottom: 10,
+        }}
+      >
+        <View
+          style={{
+            borderRadius: 45,
+            width: 50,
+            height: 50,
+            backgroundColor: "#0a66c2",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ fontSize: 25, color: "white" }}>N</Text>
+        </View>
+        <TextInput
+          style={styles.input}
+          multiline
+          onChangeText={setSearch}
+          value={search}
+          placeholder="Search User"
+        />
+      </View>
+      {search.length > 0 ? (
+        <View
+          style={{ flex: 1, backgroundColor: "white", paddingHorizontal: 16 }}
+        >
+          <FlatList
+            data={DATA}
+            renderItem={() => <FollowCard />}
+            keyExtractor={(item) => item.toString()}
+          />
+        </View>
+      ) : (
+        <FlatList
+          data={DATA}
+          renderItem={() => <Card />}
+          keyExtractor={(item) => item.toString()}
+        />
+      )}
     </View>
   );
 }
@@ -23,7 +67,10 @@ const styles = StyleSheet.create({
     paddingTop: 60,
   },
   input: {
-    height: 40,
+    textAlignVertical: "center",
+    flex: 1,
+    // height: 40,
+    // borderWidth: 1,
     padding: 10,
   },
 });
